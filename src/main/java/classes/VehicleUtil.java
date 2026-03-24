12654ceb4c616f32949b7ecbf219f3952a739490
Mikita <nikitosgeevic@gmail.com>
2026-03-24 19:29:59 +0300

    clear

--- src/main/java/classes/VehicleUtil.java
+++
@@ -1,9 +0,0 @@
-package classes;
-
-import java.util.function.Function;
-
-public class VehicleUtil {
-    private VehicleUtil(){}
-
-    public static final Function<Double, Double> GROUND_VEHICLE_Y_MOTION = _ -> 0.0;
-}
--- src/main/java/org/example/app/Launcher.java
+++
@@ -1,7 +0,0 @@
-package org.example.app;
-
-public class Launcher {
-    static void main(String[] args) {
-        javafx.application.Application.launch(Application.class, args);
-    }
-}
